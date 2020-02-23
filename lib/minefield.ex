f1a31596d0ea05de32e2e1422db0336b3fb72dd6
André Albuquerque <andre.malbuq@gmail.com>
2020-02-23 16:03:11 +0000

    Starting a repo to track changes needed for custom Nerves system
    All changes that come afterwards will be to use the custom Nerves
    system.

---
+++ lib/minefield.ex
@@ -0,0 +1,18 @@
+defmodule Minefield do
+  @moduledoc """
+  Documentation for Minefield.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Minefield.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/minefield_test.exs
@@ -0,0 +1,8 @@
+defmodule MinefieldTest do
+  use ExUnit.Case
+  doctest Minefield
+
+  test "greets the world" do
+    assert Minefield.hello() == :world
+  end
+end
