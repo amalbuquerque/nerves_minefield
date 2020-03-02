9fbe3b303af3d276338944da785b2bc7a7ff2721
André Albuquerque <andre.malbuq@gmail.com>
2020-03-02 23:17:59 +0000

    Use Nerves project USB Gadget to configure HID gadget
    Configure the device right when the application starts.

--- lib/minefield/application.ex
+++ lib/minefield/application.ex
@@ -1,39 +1,29 @@
 defmodule Minefield.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+    configure_device(target())
+
     opts = [strategy: :one_for_one, name: Minefield.Supervisor]
     children =
-      [
-        # Children for all targets
-        # Starts a worker by calling: Minefield.Worker.start_link(arg)
-        # {Minefield.Worker, arg},
-      ] ++ children(target())
+      [] ++ children(target())
 
     Supervisor.start_link(children, opts)
   end
 
-  # List all child processes to be supervised
+  def configure_device(:host), do: :no_device
+  def configure_device(_target) do
+    Minefield.Usb.Gadget.configure_device()
+  end
+
   def children(:host) do
-    [
-      # Children that only run on the host
-      # Starts a worker by calling: Minefield.Worker.start_link(arg)
-      # {Minefield.Worker, arg},
-    ]
+    []
   end
 
   def children(_target) do
-    [
-      # Children for all targets except host
-      # Starts a worker by calling: Minefield.Worker.start_link(arg)
-      # {Minefield.Worker, arg},
-    ]
+    []
   end
 
   def target() do
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Minefield.MixProject do
 
   @app :minefield
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
+  @all_targets [:rpi0, :x86_64, :custom_rpi0]
 
   def project do
     [
@@ -47,16 +47,11 @@ defmodule Minefield.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.6", targets: @all_targets},
       {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
+      {:usb_gadget, git: "https://github.com/nerves-project/usb_gadget.git", branch: "master", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.10", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.10", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.10", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.10", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.10", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.10", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.5", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.10", runtime: false, targets: :x86_64},
+      {:nerves_system_rpi0, "~> 1.9", runtime: false, targets: :rpi0},
+      {:minefield_custom_rpi0, path: "../minefield_custom_nerves_rpi0", runtime: false, targets: :custom_rpi0},
     ]
   end
 
